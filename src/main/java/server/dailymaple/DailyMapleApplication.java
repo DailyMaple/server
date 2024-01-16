7d91a6d54b19ad181df70efd25f0737ab919851b
jickDo <pocard0117@naver.com>
2024-01-17 05:21:08 +0900

    first commit

---
+++ src/main/java/server/dailymaple/DailyMapleApplication.java
@@ -0,0 +1,13 @@
+package server.dailymaple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyMapleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyMapleApplication.class, args);
+	}
+
+}
