89028fa9d38dbda1d912965c30149dd319b376d6
jickDo <pocard0117@naver.com>
2024-01-25 22:40:34 +0900

    memberRepository 생성

---
+++ src/main/java/server/dailymaple/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package server.dailymaple.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import server.dailymaple.entity.Member;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+}
