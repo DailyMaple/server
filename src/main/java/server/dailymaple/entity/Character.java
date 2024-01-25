5d19df102d434c22362052706e5be104e9719d50
jickDo <pocard0117@naver.com>
2024-01-25 21:25:21 +0900

    캐릭터 엔티티 생성

---
+++ src/main/java/server/dailymaple/entity/Character.java
@@ -0,0 +1,17 @@
+package server.dailymaple.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class Character {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "character_id")
+    Long id;
+
+    @Column(name = "character_ocid")
+    String characterId;
+}
