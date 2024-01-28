15cf4902a67db8ac080b2b2e299b9d7e0d4c1bcd
jickDo <pocard0117@naver.com>
2024-01-28 16:37:56 +0900

    feat: LoginController 구성만

---
+++ src/main/java/server/dailymaple/controller/LoginController.java
@@ -0,0 +1,13 @@
+package server.dailymaple.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+
+
+}
