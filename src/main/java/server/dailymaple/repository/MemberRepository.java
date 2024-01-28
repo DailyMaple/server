5fd9832fb4adbe6828c0214e441aadc6d6614064
jickDo <pocard0117@naver.com>
2024-01-28 17:07:31 +0900

    feat: 멤버 레포에서 data jpa를 이용하는 아이디 체크 로직 생성

--- src/main/java/server/dailymaple/repository/MemberRepository.java
+++ src/main/java/server/dailymaple/repository/MemberRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import server.dailymaple.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
+    boolean existsByAccountId(String accountId);
 }
