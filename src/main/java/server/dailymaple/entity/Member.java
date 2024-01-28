74f10d476399dbe0811d527a3d14e34822be8011
jickDo <pocard0117@naver.com>
2024-01-28 14:07:01 +0900

    refactor: member 엔티티 구조 개선 - 양방향 연관을 단방향으로 변경

--- src/main/java/server/dailymaple/entity/Member.java
+++ src/main/java/server/dailymaple/entity/Member.java
@@ -3,20 +3,14 @@ package server.dailymaple.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Getter
 @Entity
 public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "member_id")
+    @Column
     Long id;
-
-    @OneToMany(mappedBy = "character", cascade = CascadeType.ALL, orphanRemoval = true)
-    List<Character> characteList = new ArrayList<>();
 
     String accountId;
 
