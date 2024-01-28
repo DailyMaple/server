d3d47089d937bc447c29551707d29b0e06101fb4
jickDo <pocard0117@naver.com>
2024-01-28 16:38:17 +0900

    feat: SignUpController 구성

---
+++ src/main/java/server/dailymaple/controller/SignUpController.java
@@ -0,0 +1,21 @@
+package server.dailymaple.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import server.dailymaple.dto.MemberDto;
+import server.dailymaple.service.LoginService;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/signup")
+public class SignUpController {
+    private final LoginService loginService;
+
+    @PostMapping("")
+    public Long signUp(@RequestBody MemberDto memberDto){
+        return loginService.signUp(memberDto.accountId(), memberDto.password());
+    }
+}
