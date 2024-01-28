bc511b00b21a4582e083c760383d3f63bbe1f66f
jickDo <pocard0117@naver.com>
2024-01-28 16:23:30 +0900

    feat: loginService signUp 로직 구현

--- src/main/java/server/dailymaple/service/LoginService.java
+++ src/main/java/server/dailymaple/service/LoginService.java
@@ -2,6 +2,8 @@ package server.dailymaple.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import server.dailymaple.dto.MemberDto;
+import server.dailymaple.entity.Member;
 import server.dailymaple.repository.MemberRepository;
 
 @Service
@@ -10,7 +12,10 @@ public class LoginService {
     private final MemberRepository memberRepository;
 
     public Long signUp(String accountId, String password){
-
+        //ToDo 페스워드 암호화
+        Member member = Member.createMember(accountId,password);
+        memberRepository.save(member);
+        return member.getId();
     }
 
     public void validId(String accountId){
