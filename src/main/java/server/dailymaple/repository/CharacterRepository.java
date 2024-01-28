2fed8d628f8a7d64c4fa32ab6202994af448698b
jickDo <pocard0117@naver.com>
2024-01-28 14:38:29 +0900

    refactor: character 레포 명 profile레포로 변경

--- src/main/java/server/dailymaple/repository/CharacterRepository.java
+++
@@ -1,8 +0,0 @@
-package server.dailymaple.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import server.dailymaple.entity.Character;
-import server.dailymaple.entity.Member;
-
-public interface CharacterRepository extends JpaRepository<Character,Long> {
-}
