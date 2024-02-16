ad1e66fac7a6d2369869e49339fb47ceb6de62d7
jickDo <pocard0117@naver.com>
2024-02-16 12:38:51 +0900

    setting: cors 문제 해결

--- src/main/java/server/dailymaple/Configuration/WebConfig.java
+++ src/main/java/server/dailymaple/Configuration/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3030","http://localhost:8080")
+                .allowedOrigins("http://13.209.150.31:8080","http://13.209.150.31:3030")
                 .allowedMethods("GET", "POST")
                 .maxAge(3000);
     }
