e235f69ea50182e7297d8240b3d03a366abe42f8
jickDo <pocard0117@naver.com>
2024-02-17 18:27:21 +0900

    feature: BaseException 생성

---
+++ src/main/java/server/dailymaple/utils/exception/BaseException.java
@@ -0,0 +1,16 @@
+package server.dailymaple.utils.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+
+public class BaseException extends RuntimeException{
+
+    private final HttpStatus httpStatus;
+    public BaseException(HttpStatus httpStatus, String message) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+
+
+}
