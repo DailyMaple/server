476a928305ff4f7c1134c1befc7138e470c9a814
jickDo <pocard0117@naver.com>
2024-02-17 19:18:24 +0900

    feature: 비밀번호 유효성 검증

--- src/main/java/server/dailymaple/service/LoginService.java
+++ src/main/java/server/dailymaple/service/LoginService.java
@@ -3,28 +3,38 @@ package server.dailymaple.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import server.dailymaple.dto.LoginResponseDto;
+import server.dailymaple.entity.Member;
 import server.dailymaple.repository.MemberRepository;
 import server.dailymaple.utils.exception.BaseException;
+import server.dailymaple.utils.exception.BaseExceptionCode;
 
 @Service
 @RequiredArgsConstructor
 public class LoginService {
     private final MemberRepository memberRepository;
 
-    public Long login(LoginResponseDto loginResponseDto){
-        if(checkValidLogin(loginResponseDto.accountId())){
-
-        }
-
-
+    public Long login(LoginResponseDto loginResponseDto) {
+        checkValidAccountId(loginResponseDto);
+        return checkValidPassword(loginResponseDto);
     }
 
-    public boolean checkValidLogin(String accountId){
+    public Long checkValidPassword(LoginResponseDto loginResponseDto){
+        Member member = memberRepository.findByAccountId(loginResponseDto.accountId());
         try {
-            return memberRepository.existsByAccountId(accountId);
-        }catch (BaseException e){
+            if(member.getPassword().equals(loginResponseDto.password())){
+                return member.getId();
+            };
+        }
+        catch (BaseException e){
             throw e;
         }
+    }
+
+    public boolean checkValidAccountId(LoginResponseDto loginResponseDto) {
+            if (memberRepository.existsByAccountId(loginResponseDto.accountId())){
+                throw new BaseException(BaseExceptionCode.INVALID_USER.getHttpStatus(),BaseExceptionCode.INVALID_USER.getMessage());
+            }
+            return true;
     }
 
 }
