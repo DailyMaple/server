1a9b2c374338bc5c7fbc30c940b60a0ca5e245e5
jickDo <pocard0117@naver.com>
2024-02-17 20:23:35 +0900

    refactor: 로그인 api생성

--- src/main/java/server/dailymaple/controller/LoginController.java
+++ src/main/java/server/dailymaple/controller/LoginController.java
@@ -1,13 +1,22 @@
 package server.dailymaple.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import server.dailymaple.dto.LoginResponseDto;
+import server.dailymaple.service.LoginService;
 
+@Tag(name = "Login", description = "Login Api")
 @RestController
+@AllArgsConstructor
 @RequestMapping("/login")
 public class LoginController {
+    private final LoginService loginService;
 
-
-
+    @Operation(summary = "로그인", description = "아아디와 비밀번호를 받아 로그인가능 여부를 판별한다")
+    @PostMapping("")
+    public Long login(@RequestBody LoginResponseDto loginResponseDto){
+        return loginService.login(loginResponseDto);
+    }
 }
