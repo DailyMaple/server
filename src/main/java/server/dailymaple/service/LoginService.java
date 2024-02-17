53395f90b22d245c8c6a566f4942f66526ffbee6
jickDo <pocard0117@naver.com>
2024-02-17 18:40:23 +0900

    feature: 로그인 valid 에러 처리 생성

--- src/main/java/server/dailymaple/service/LoginService.java
+++ src/main/java/server/dailymaple/service/LoginService.java
@@ -2,24 +2,29 @@ package server.dailymaple.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import server.dailymaple.dto.AccountIdResponse;
-import server.dailymaple.dto.MemberDto;
-import server.dailymaple.entity.Member;
+import server.dailymaple.dto.LoginResponseDto;
 import server.dailymaple.repository.MemberRepository;
+import server.dailymaple.utils.exception.BaseException;
 
 @Service
 @RequiredArgsConstructor
 public class LoginService {
     private final MemberRepository memberRepository;
 
-    public Long signUp(MemberDto memberDto){
-        //ToDo 페스워드 암호화
-        Member member = Member.createMember(memberDto.accountId(),memberDto.password());
-        memberRepository.save(member);
-        return member.getId();
+    public Long login(LoginResponseDto loginResponseDto){
+        if(checkValidLogin(loginResponseDto.accountId())){
+
+        }
+
+
     }
 
-    public boolean checkIdExist(AccountIdResponse accountIdResponse){
-        return memberRepository.existsByAccountId(accountIdResponse.accountId());
+    public boolean checkValidLogin(String accountId){
+        try {
+            return memberRepository.existsByAccountId(accountId);
+        }catch (BaseException e){
+            throw e;
+        }
     }
+
 }
