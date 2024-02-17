51723d2fa85575109297d493ee382cb5a4be515a
jickDo <pocard0117@naver.com>
2024-02-17 18:29:42 +0900

    refactor: BaseException getter추가

--- src/main/java/server/dailymaple/utils/exception/BaseException.java
+++ src/main/java/server/dailymaple/utils/exception/BaseException.java
@@ -3,7 +3,7 @@ package server.dailymaple.utils.exception;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-
+@Getter
 public class BaseException extends RuntimeException{
 
     private final HttpStatus httpStatus;
