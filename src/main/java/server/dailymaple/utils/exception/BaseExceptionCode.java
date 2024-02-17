d86f09470be3133b91f5d68e9b4499a10b8fa0d0
jickDo <pocard0117@naver.com>
2024-02-17 20:23:01 +0900

    feature: 비밀번호 다른 커스텀 에러

--- src/main/java/server/dailymaple/utils/exception/BaseExceptionCode.java
+++ src/main/java/server/dailymaple/utils/exception/BaseExceptionCode.java
@@ -7,6 +7,10 @@ import org.springframework.http.HttpStatus;
 public enum BaseExceptionCode {
 
     INVALID_USER(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다."),
+
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED,"비밀번호가 일치하지 않습니다"),
+
+
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
     AUTHORIZATION_HEADER_NULL(HttpStatus.BAD_REQUEST, "인증 헤더가 null입니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러입니다.");
