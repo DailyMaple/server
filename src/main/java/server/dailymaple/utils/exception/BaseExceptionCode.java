493dbf1c30c007f360f57a653fa24e6c83786924
jickDo <pocard0117@naver.com>
2024-02-17 18:35:19 +0900

    feature: enum 타입 커스텀 에러 생성

---
+++ src/main/java/server/dailymaple/utils/exception/BaseExceptionCode.java
@@ -0,0 +1,22 @@
+package server.dailymaple.utils.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum BaseExceptionCode {
+
+    INVALID_USER(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다."),
+    INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    AUTHORIZATION_HEADER_NULL(HttpStatus.BAD_REQUEST, "인증 헤더가 null입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러입니다.");
+    private final String message;
+    private final HttpStatus httpStatus;
+
+
+    BaseExceptionCode(HttpStatus httpCode, String message) {
+        this.httpStatus = httpCode;
+        this.message = message;
+    }
+}
+
