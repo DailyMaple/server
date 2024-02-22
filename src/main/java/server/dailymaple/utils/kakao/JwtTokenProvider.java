fd75d5b5f35a3552c28df7958be41e494b59f2c1
jickDo <pocard0117@naver.com>
2024-02-23 00:06:57 +0900

    feature: 토큰 생성과 페이로드 get함수

---
+++ src/main/java/server/dailymaple/utils/kakao/JwtTokenProvider.java
@@ -0,0 +1,52 @@
+package server.dailymaple.utils.kakao;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Component;
+import server.dailymaple.utils.exception.BaseException;
+import server.dailymaple.utils.exception.BaseExceptionCode;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${security.jwt.token.secret-key}")
+    String secretKey;
+    @Value("${security.jwt.token.expire-length}")
+    private Long validityInMilliseconds;
+
+    private SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+    public String createToken(String payload) {
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+        return Jwts.builder()
+                .setSubject(payload)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String getPayload(String token) {
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody()
+                    .getSubject();
+        } catch (JwtException e){
+            throw new BaseException(HttpStatus.BAD_REQUEST,"페이로드를 가져오는데 실패했습니다");
+        }
+    }
+}
