df2b172bd76b72dc9df3160a7a297ab4db88ef84
jickDo <pocard0117@naver.com>
2024-02-22 21:43:59 +0900

    feature: id추출용 dto

---
+++ src/main/java/server/dailymaple/utils/kakao/constants/KakaoOauthConstants.java
@@ -0,0 +1,6 @@
+package server.dailymaple.utils.kakao.constants;
+
+public class KakaoOauthConstants {
+    public static final String KAKAO_URL = "https://kapi.kakao.com";
+    public static final String KAKAO_USER_INFO = "/v2/user/me";
+}
