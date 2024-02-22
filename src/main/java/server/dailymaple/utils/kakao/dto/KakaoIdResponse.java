c6e390b955e05e7118891345f50a389cedf6954a
jickDo <pocard0117@naver.com>
2024-02-22 21:45:13 +0900

    feature: id추출용 dto

---
+++ src/main/java/server/dailymaple/utils/kakao/dto/KakaoIdResponse.java
@@ -0,0 +1,12 @@
+package server.dailymaple.utils.kakao.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+@Schema(title = "카카오 토큰에서 id값 만을 받아오기 위한 dto")
+public record KakaoIdResponse(
+        @Schema(title = "카카오 유저 id", example = "")
+        String id
+) {
+
+}
