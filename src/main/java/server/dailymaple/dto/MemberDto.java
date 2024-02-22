840f36e8d0e4a9d8461aad96939bff37bf673264
jickDo <pocard0117@naver.com>
2024-02-22 22:56:01 +0900

    chore: Member Dto에 로그인타입 생성

--- src/main/java/server/dailymaple/dto/MemberDto.java
+++ src/main/java/server/dailymaple/dto/MemberDto.java
@@ -1,12 +1,14 @@
 package server.dailymaple.dto;
 
 import lombok.Builder;
+import server.dailymaple.constant.LoginType;
 
 @Builder
 public record MemberDto(
         Long id,
         String accountId,
         String password,
+        LoginType type,
         boolean deleted
 
 ) {
