3f3290cb9c005318b83c00b92212697e86961f26
jickDo <pocard0117@naver.com>
2024-02-22 21:52:08 +0900

    feature: httpinterface를 사용한 외부 api와 통신

---
+++ src/main/java/server/dailymaple/utils/kakao/client/KakaoAccessClient.java
@@ -0,0 +1,23 @@
+package server.dailymaple.utils.kakao.client;
+
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.service.annotation.PostExchange;
+import server.dailymaple.utils.kakao.constants.KakaoOauthConstants;
+import server.dailymaple.utils.kakao.dto.KakaoIdResponse;
+
+@HttpExchange(KakaoOauthConstants.KAKAO_URL)
+public interface KakaoAccessClient {
+    @PostExchange(KakaoOauthConstants.KAKAO_USER_INFO)
+    public KakaoIdResponse getKakaoUserId(
+            @RequestHeader("Authorization") String authorization
+    );
+
+    @PostExchange(KakaoOauthConstants.KAKAO_USER_INFO)
+    public KakaoInfoResponse getKakaoUserInfo(
+            @RequestHeader("Authorization") String authorization
+    );
+}
+
+
+
