0d0f0eed183b5e45747506306a9d49425102f226
jickDo <pocard0117@naver.com>
2024-02-26 22:13:53 +0900

    feature: http interface를 받는 토큰받는 서비스 구현

--- src/main/java/server/dailymaple/oauth/service/KakaoService.java
+++ src/main/java/server/dailymaple/oauth/service/KakaoService.java
@@ -1,12 +1,13 @@
 package server.dailymaple.oauth.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import server.dailymaple.oauth.kakao.client.KakaoAccessClient;
-import server.dailymaple.oauth.service.dto.KakaoAuthorizationRequest;
-import server.dailymaple.oauth.service.dto.KakaoAuthorizationResponse;
+import server.dailymaple.oauth.service.dto.KakaoTokenRequest;
+import server.dailymaple.oauth.service.dto.KakaoTokenResponse;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -16,13 +17,13 @@ public class KakaoService {
 
     @Value("${spring.security.oauth2.client.registration.kakao.client_id}")
     private String CLIENT_ID;
-    private final static String CODE = "code";
-    private final static String REDIRECT_URL = "https://example.com/oauth";
+    private static final String GRANT_TYPE = "authorization_code";
+    private static final String REDIRECT_URL = "https://example.com/oauth";
+    private static final String Content_type = "application/x-www-form-urlencoded;charset=utf-8";
 
 
-
-
-
-//client.getKakaoAuthorizationCode(CLIENT_ID,REDIRECT_URL,CODE);
+    public KakaoTokenResponse getOauthToken(KakaoTokenRequest req){
+        return client.getKakaoAuthorizationCode(GRANT_TYPE,CLIENT_ID,REDIRECT_URL,req.code());
+    }
 
 }
