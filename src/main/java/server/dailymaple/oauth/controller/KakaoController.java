a592e61d5a2083e96108a759977d56b89d4e9928
jickDo <pocard0117@naver.com>
2024-02-26 17:24:59 +0900

    chore: 기본 api 삭제

--- src/main/java/server/dailymaple/oauth/controller/KakaoController.java
+++ src/main/java/server/dailymaple/oauth/controller/KakaoController.java
@@ -17,20 +17,4 @@ public class KakaoController {
 
     private final KakaoService service;
 
-    @GetMapping("/oauth/authorize")
-    public String getKakaoAuthorizationCode(
-            @RequestParam("client_id") String clientId,
-            @RequestParam("redirect_uri") String redirectUrl,
-            @RequestParam("response_type") String responseType
-    ) {
-        return "redirect:https://kauth.kakao.com/oauth/authorize"
-                + "?client_id=" + clientId
-                + "&redirect_uri=" + redirectUrl
-                + "&response_type=" + responseType;
-    }
-
-    @GetMapping("/example.com/oauth")
-    public String kakaoCallback(@RequestParam String code) {
-        return "Received code: " + code;
-    }
 }
