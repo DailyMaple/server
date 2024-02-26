56a28e880c72c7b4669bf4db5cdff478dbd94cd5
jickDo <pocard0117@naver.com>
2024-02-26 22:14:06 +0900

    feature: 인가 토큰받는 api 구현

--- src/main/java/server/dailymaple/oauth/controller/KakaoController.java
+++ src/main/java/server/dailymaple/oauth/controller/KakaoController.java
@@ -2,19 +2,22 @@ package server.dailymaple.oauth.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
-import org.springframework.web.util.UriComponentsBuilder;
 import server.dailymaple.oauth.service.KakaoService;
-import server.dailymaple.oauth.service.dto.KakaoAuthorizationRequest;
-import server.dailymaple.oauth.service.dto.KakaoAuthorizationResponse;
+import server.dailymaple.oauth.service.dto.KakaoTokenRequest;
+import server.dailymaple.oauth.service.dto.KakaoTokenResponse;
 
 @Tag(name = "카카오 oauth", description = "카카오 oauth api")
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RequestMapping("/oauth/kakao")
 public class KakaoController {
 
     private final KakaoService service;
 
+    @PostMapping("")
+    public KakaoTokenResponse getToken(KakaoTokenRequest req){
+        return service.getOauthToken(req);
+    }
 }
