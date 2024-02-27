925ac89acce06212903752074fceafec723a0414
jickDo <pocard0117@naver.com>
2024-02-27 18:02:03 +0900

    feature: userId를 가져오는 httpinterface 로직추가

--- src/main/java/server/dailymaple/oauth/kakao/client/KakaoAccessClient.java
+++ src/main/java/server/dailymaple/oauth/kakao/client/KakaoAccessClient.java
@@ -1,18 +1,29 @@
 package server.dailymaple.oauth.kakao.client;
 
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.annotation.PostExchange;
+import server.dailymaple.oauth.kakao.constants.KakaoOauthUrl;
 import server.dailymaple.oauth.service.dto.KakaoTokenResponse;
+import server.dailymaple.oauth.service.dto.KakaoUserIdResponse;
 
-@HttpExchange("https://kauth.kakao.com")
+@HttpExchange("")
 public interface KakaoAccessClient {
 
-    @PostExchange(contentType = "application/x-www-form-urlencoded",url = "/oauth/token")
+
+    @PostExchange(KakaoOauthUrl.OAUTH_TOKEN_MAKE_URL)
     KakaoTokenResponse getKakaoAuthorizationCode(
             @RequestParam("grant_type") String grantType,
             @RequestParam("client_id") String clientId,
             @RequestParam("redirect_uri") String redirectUri,
             @RequestParam("code") String code
     );
+
+    @GetExchange(KakaoOauthUrl.OAUTH_GET_USERINFO_URL)
+    KakaoUserIdResponse getKakaoUserId(
+            @RequestHeader String authorization
+    );
+
 }
