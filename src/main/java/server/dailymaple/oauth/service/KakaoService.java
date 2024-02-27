97ef12f33ef82a976648f188b92a301c6f5e513d
jickDo <pocard0117@naver.com>
2024-02-27 18:22:59 +0900

    chore: 회원가입 로직 로그인 로직생성

--- src/main/java/server/dailymaple/oauth/service/KakaoService.java
+++ src/main/java/server/dailymaple/oauth/service/KakaoService.java
@@ -2,26 +2,43 @@ package server.dailymaple.oauth.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import server.dailymaple.constant.LoginType;
+import server.dailymaple.entity.Member;
 import server.dailymaple.oauth.kakao.client.KakaoAccessClient;
 import server.dailymaple.oauth.kakao.constants.KakaoOauthConstant;
 import server.dailymaple.oauth.service.dto.KakaoTokenRequest;
 import server.dailymaple.oauth.service.dto.KakaoTokenResponse;
 import server.dailymaple.oauth.service.dto.KakaoUserIdResponse;
-
-import java.util.HashMap;
+import server.dailymaple.repository.MemberRepository;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class KakaoService {
     private final KakaoAccessClient client;
+    private final MemberRepository repository;
 
     @Value("${spring.security.oauth2.client.registration.kakao.client_id}")
     private String CLIENT_ID;
 
+    @Transactional
+    public String login(KakaoTokenResponse req){
+        String userId = String.valueOf(getUserId(req).id());
+        return (repository.findByAccountId(userId)== null) ? signUp(req,userId) : userId;
+    }
+
+    @Transactional
+    public String signUp(KakaoTokenResponse req, String userId){
+        Member newMember = Member.builder()
+                .accountId(userId)
+                .password(null)
+                .type(LoginType.KAKAO)
+                .deleted(false)
+                .build();
+        return repository.save(newMember).getAccountId();
+    }
 
     public KakaoTokenResponse getOauthToken(KakaoTokenRequest req){
         return client.getKakaoAuthorizationCode(KakaoOauthConstant.GRANT_TYPE,CLIENT_ID,KakaoOauthConstant.REDIRECT_URL,req.code());
