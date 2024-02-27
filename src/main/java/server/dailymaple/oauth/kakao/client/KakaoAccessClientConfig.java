b5ac46dd45f6a442deb42ddedd397bed333fadc9
jickDo <pocard0117@naver.com>
2024-02-27 18:01:30 +0900

    chore: 기본헤더 설정변경

--- src/main/java/server/dailymaple/oauth/kakao/client/KakaoAccessClientConfig.java
+++ src/main/java/server/dailymaple/oauth/kakao/client/KakaoAccessClientConfig.java
@@ -3,6 +3,8 @@ package server.dailymaple.oauth.kakao.client;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -11,7 +13,9 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 public class KakaoAccessClientConfig {
     @Bean
     public static KakaoAccessClient kakaoOpenApiClient() {
-        WebClient webClient = WebClient.builder().build();
+        WebClient webClient = WebClient.builder()
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                .build();
         HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(WebClientAdapter.create(webClient)).build();
         return factory.createClient(KakaoAccessClient.class);
     }
