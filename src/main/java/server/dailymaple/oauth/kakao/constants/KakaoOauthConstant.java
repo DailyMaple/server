83d7bc2096f2fe1ed16bbb5a059b3243e52df195
jickDo <pocard0117@naver.com>
2024-02-27 18:00:46 +0900

    feature: 기존 상수 클래스로 따로 관리

---
+++ src/main/java/server/dailymaple/oauth/kakao/constants/KakaoOauthConstant.java
@@ -0,0 +1,7 @@
+package server.dailymaple.oauth.kakao.constants;
+
+public class KakaoOauthConstant {
+    public static final String GRANT_TYPE = "authorization_code";
+    public static final String REDIRECT_URL = "https://example.com/oauth";
+
+}
---
+++ src/main/java/server/dailymaple/oauth/kakao/constants/KakaoOauthUrl.java
@@ -0,0 +1,7 @@
+package server.dailymaple.oauth.kakao.constants;
+
+public class KakaoOauthUrl {
+    public static final String OAUTH_TOKEN_MAKE_URL ="https://kauth.kakao.com/oauth/token";
+    public static final String OAUTH_GET_USERINFO_URL ="https://kapi.kakao.com/v2/user/me";
+
+}
