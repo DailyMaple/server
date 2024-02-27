5038170c162279b013a048232a2a81bb9841f788
jickDo <pocard0117@naver.com>
2024-02-27 18:22:15 +0900

    feature: 로그인 타입 enum타입선언

--- src/main/java/server/dailymaple/oauth/controller/KakaoController.java
+++ src/main/java/server/dailymaple/oauth/controller/KakaoController.java
@@ -1,12 +1,16 @@
 package server.dailymaple.oauth.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import server.dailymaple.oauth.service.KakaoService;
 import server.dailymaple.oauth.service.dto.KakaoTokenRequest;
 import server.dailymaple.oauth.service.dto.KakaoTokenResponse;
+import server.dailymaple.oauth.service.dto.KakaoUserIdResponse;
+
+import java.util.HashMap;
 
 @Tag(name = "카카오 oauth", description = "카카오 oauth api")
 @RestController
@@ -19,5 +23,16 @@ public class KakaoController {
     @PostMapping("")
     public KakaoTokenResponse getToken(KakaoTokenRequest req){
         return service.getOauthToken(req);
+    }
+
+    @PostMapping("/test")
+    public KakaoUserIdResponse getUserId(@RequestBody KakaoTokenResponse req){
+        return service.getUserId(req);
+    }
+
+    @Operation(summary = "Oauth 타입 별 로그인", description = "각 Oauth 타입에 맞는 로그인을 진행한 뒤 AccessToken을 반환한다.")
+    @PostMapping("/login")
+    public String login(@RequestBody KakaoTokenResponse req){
+        return service.login(req);
     }
 }
