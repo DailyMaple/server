cdf0194e8ff063e3cc759397ba9708e13589f563
jickDo <pocard0117@naver.com>
2024-02-28 22:15:04 +0900

    chore: 리다이렉트 주소변경

--- src/main/java/server/dailymaple/oauth/kakao/constants/KakaoOauthConstant.java
+++ src/main/java/server/dailymaple/oauth/kakao/constants/KakaoOauthConstant.java
@@ -2,6 +2,6 @@ package server.dailymaple.oauth.kakao.constants;
 
 public class KakaoOauthConstant {
     public static final String GRANT_TYPE = "authorization_code";
-    public static final String REDIRECT_URL = "https://example.com/oauth";
+    public static final String REDIRECT_URL = "http://127.0.0.1:8888/kakao-login";
 
 }
