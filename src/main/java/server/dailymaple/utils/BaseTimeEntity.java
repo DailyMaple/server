e8c473dee66725a89317c30277ca727a59635ced
jickDo <pocard0117@naver.com>
2024-02-29 22:07:03 +0900

    chore: BaseTimeEntity 변수명 변경

--- src/main/java/server/dailymaple/utils/BaseTimeEntity.java
+++ src/main/java/server/dailymaple/utils/BaseTimeEntity.java
@@ -15,9 +15,9 @@ import java.time.LocalDateTime;
 public abstract class BaseTimeEntity{
 
     @CreatedDate
-    private LocalDateTime createdDateTime;
+    private LocalDateTime createdAt;
 
     // 조회한 Entity 값을 변경할 때 시간이 자동 저장됩니다.
     @LastModifiedDate
-    private LocalDateTime modifiedDateTime;
+    private LocalDateTime updatedAt;
 }
